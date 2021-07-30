30add7ec0afa56b9bf4ac5ad560f37ffb1c2f8a1
putrasatr <satriaputrapratama2002@gmail.com>
2021-07-30 09:30:26 +0700

    Beautify Cosmetics

--- src/routers/DrawerNavigator/index.js
+++ src/routers/DrawerNavigator/index.js
@@ -1,29 +1,17 @@
 import React from 'react'
 import { View, Text } from 'react-native';
-import { createDrawerNavigator, DrawerItem, DrawerItemList, DrawerContentScrollView } from '@react-navigation/drawer';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 import Icon from 'react-native-vector-icons/dist/MaterialCommunityIcons'
 
-import { DrawerRouters } from '../../components';
+import { DrawerRouters, DrawerList } from '../../components';
 import { styles } from './styles';
-import { useThemeContext } from '../../context/ThemeProvider';
-import { Purple, White } from '../../assets/colors';
+import { White } from '../../assets/colors';
 
 const Drawer = createDrawerNavigator();
 
-const ListDrawer = (props) => {
-    const { theme } = useThemeContext()
-    const isDarkTheme = theme === "dark"
-    return (
-        <DrawerContentScrollView {...props} style={{ backgroundColor: isDarkTheme ? "black" : "white" }}>
-            <DrawerItemList activeBackgroundColor={Purple} activeTintColor={White} {...props} style={{ marginTop: 10 }} />
-            <DrawerItem {...props} labelStyle={{ color: isDarkTheme ? White : "black" }} label="Logout" onPress={() => { }} />
-        </DrawerContentScrollView>
-
-    );
-}
 export default function DrawerNavigator() {
     return (
-        <Drawer.Navigator drawerContent={props => <ListDrawer  {...props} />}>
+        <Drawer.Navigator drawerContent={props => <DrawerList  {...props} />}>
             {DrawerRouters.map(({ name, Component, title, iconName }, i) => (
                 <Drawer.Screen key={i} name={name} component={Component} options={{
                     drawerLabel: ({ focused }) => (
