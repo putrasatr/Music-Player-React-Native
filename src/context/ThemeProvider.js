2a122fb3ecb4cd5b8faa6bb6b136a15ffb4f1e3b
putrasatr <satriaputrapratama2002@gmail.com>
2021-07-30 09:28:58 +0700

    Bug fixong Thme Provider

--- src/context/ThemeProvider.js
+++ src/context/ThemeProvider.js
@@ -1,17 +1,29 @@
-import React, { createContext, useState, useContext } from "react"
+import React, { createContext, useState, useContext, useEffect } from "react"
+import { useDispatch } from "react-redux";
 
+import { SecureAsyncStorage } from "../helpers";
 
 const ThemeContext = createContext({
-    setTheme: () => { },
-    theme: "dark"
+    setThemeContext: () => { },
+    theme: undefined
 })
 
 export const useThemeContext = () => useContext(ThemeContext);
 
-export default function AudioProvider({ children }) {
+export default function ThemeProvider({ children }) {
     const [theme, setTheme] = useState("dark")
+    const setThemeContext = async (value) => {
+        setTheme(value)
+        await SecureAsyncStorage.setItem("theme", value)
+    }
+    useEffect(async () => {
+        const value = await SecureAsyncStorage.getItem("theme")
+        if (value) setThemeContext(value)
+        else await SecureAsyncStorage.setItem("theme", theme)
+        return () => clearTimeout()
+    }, [SecureAsyncStorage])
     return (
-        <ThemeContext.Provider value={{ setTheme, theme }}>
+        <ThemeContext.Provider value={{ setThemeContext, theme }}>
             {children}
         </ThemeContext.Provider>
     )
