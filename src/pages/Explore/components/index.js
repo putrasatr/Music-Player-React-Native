460581e0b7ec300ef112abe220ae6c578da8ff59
putrasatr <satriaputrapratama2002@gmail.com>
2021-08-02 17:16:18 +0700

    Fetching maping data

--- src/pages/Explore/components/index.js
+++ src/pages/Explore/components/index.js
@@ -1,36 +1,55 @@
-import React from "react"
-import { View, FlatList, Text, Image, ScrollView } from "react-native"
+import React, { useState, useEffect } from "react"
+import { View, FlatList, Text, Image, ScrollView, ActivityIndicator } from "react-native"
+import { useDispatch, useSelector } from "react-redux"
 
 import { Header, Layout } from "../../../components"
 import { useThemeContext } from "../../../context"
-import { White } from "../../../assets/colors"
+import { White, Purple, FillgroundColor } from "../../../assets/colors"
 import { styles } from "./styles"
+import { loading } from "../../../services/actions/loading"
+import { getSongPerArtist } from "../../../helpers"
 
 export default function Explore() {
     const { theme } = useThemeContext()
+    const [songs, setSongs] = useState(null)
+    const dispatch = useDispatch()
+    const { data, isLoading } = useSelector(({ audio, loading }) => ({ ...audio, ...loading }))
     const isDarkTheme = theme === "dark"
+
+    useEffect(async () => {
+        dispatch(loading(true))
+        const res = await getSongPerArtist(data)
+        setSongs(res)
+        dispatch(loading(false))
+        return () => clearTimeout()
+    }, [dispatch])
+    const Content = ({ songObj }) => {
+        let col = []
+        let i = 0
+        for (const key in songObj) {
+            col.push(<Text
+                key={i}
+                style={{
+                    color: isDarkTheme ? White : "black"
+                }}>{key}</Text>)
+            i++
+        }
+        return (
+            <View>
+                {col}
+            </View>
+        )
+    }
+    console.log(isLoading)
     return (
         <Layout>
             <Header isDarkTheme={isDarkTheme} />
-            <View>
-                <Text style={{
-                    color: isDarkTheme ? White : "black"
-                }}>Search</Text>
-            </View>
-            <View style={{
-                width: "100%",
-                height: "80%",
-                flexDirection: "column",
-                justifyContent: "center",
-                alignItems: "center"
-            }}>
-                <Image
-                    style={{
-                        width: 200,
-                        height:"80%"
-                    }}
-                    source={require("../../../assets/images/listen.png")} />
-            </View>
+            {isLoading
+                ? <ActivityIndicator size="large" color={Purple} style={{ width: "100%", height: "85%" }} />
+                : songs
+                    ? <Content songObj={songs} />
+                    : <Text>No Songs</Text>}
+
         </Layout>
     )
 }
