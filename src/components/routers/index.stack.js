a1e5880c5947aaf0a011ecb611f4bd6cb9552b97
putrasatr <satriaputrapratama2002@gmail.com>
2021-08-12 12:07:10 +0700

    modify components router

--- src/components/routers/index.stack.js
+++ src/components/routers/index.stack.js
@@ -1,9 +1,10 @@
 import { DrawerScreen } from "../../routers"
-import { PlayRoom, SearchScreen, SongsListRoom } from ".."
+import { PlayRoom, SearchScreen, SongsListRoom,OnBoardingScreen } from ".."
 
 export const StackRouter = [
     { name: "HomeScreen", Component: DrawerScreen },
     { name: "SearchScreen", Component: SearchScreen },
     { name: "PlayRoomScreen", Component: PlayRoom },
-    { name: "SongsListScreen", Component: SongsListRoom }
+    { name: "SongsListScreen", Component: SongsListRoom },
+    // { name: "OnBoardingScreen", Component: OnBoardingScreen}
 ]
