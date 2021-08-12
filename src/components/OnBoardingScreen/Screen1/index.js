dc1be34f4a7ef2fd06bf426643616245c9ba6d6a
putrasatr <satriaputrapratama2002@gmail.com>
2021-08-12 12:06:42 +0700

    Creating Onboarding Screen UI

---
+++ src/components/OnBoardingScreen/Screen1/index.js
@@ -0,0 +1,1 @@
+export { default as Screen1 } from './components'
---
+++ src/components/OnBoardingScreen/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View } from "react-native";
+
+import { Screen1 } from './Screen1';
+
+export default function Components() {
+    return (
+        <View>
+            <Screen1 />
+        </View>
+    )
+}
